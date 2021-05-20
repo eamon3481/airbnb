81ac34c976db30e72a061e078d646b9d8632d412
woody <woojihye2339@gmail.com>
2021-05-21 00:45:51 +0900

    [#10] feature : 호텔 예약 디테일 정보 요청 처리 구현 - mock
    > ReservedDetailResponse 클래스 구현
      - 구체적으로 특정 예약 정보를 담는 dto 클래스

--- be/src/main/java/com/team09/airbnb/controller/ReserveController.java
+++ be/src/main/java/com/team09/airbnb/controller/ReserveController.java
@@ -1,8 +1,10 @@
 package com.team09.airbnb.controller;
 
+import com.team09.airbnb.response.ReservedDetailResponse;
 import com.team09.airbnb.response.ReservedResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -17,6 +19,12 @@ public class ReserveController {
         ReservedResponse response2 = new ReservedResponse(1L, "Specious Central Itaewon – NEWTRO House", new Date(2021, 06, 11), new Date(2021, 06, 15), 2, 1, 1);
 
         return Arrays.asList(response1, response2);
+    }
+
+    @GetMapping("/{reservedId}")
+    public ReservedDetailResponse reservedDetail(@PathVariable Long reservedId) {
+        return new ReservedDetailResponse(1L, "C1 New apartment right next to Gangnam Station", BigDecimal.valueOf(79000), false, 37.498063, 127.030187, 4, Arrays.asList("주방", "무선인터넷", "에어컨", "헤어드라이"), "Baba", Arrays.asList("https://team09-airbnb.s3.ap-northeast-2.amazonaws.com/airbnb1-1.png", "https://team09-airbnb.s3.ap-northeast-2.amazonaws.com/airbnb1-2.png", "https://team09-airbnb.s3.ap-northeast-2.amazonaws.com/airbnb1-3.png"),
+                1L, new Date(2021, 06, 01), new Date(2021, 06, 10), 2, 0, 0);
     }
 
 
---
+++ be/src/main/java/com/team09/airbnb/response/ReservedDetailResponse.java
@@ -0,0 +1,49 @@
+package com.team09.airbnb.response;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public class ReservedDetailResponse extends HotelDetailResponse {
+
+    private final Long reservedId;
+    private final Date checkin;
+    private final Date checkout;
+    private final int adults;
+    private final int children;
+    private final int infants;
+
+    public ReservedDetailResponse(Long hotelId, String title, BigDecimal price, boolean wishlist, double longitude, double latitude, int rate, List<String> details, String host, List<String> imgs, Long reservedId, Date checkin, Date checkout, int adults, int children, int infants) {
+        super(hotelId, title, price, wishlist, longitude, latitude, rate, details, host, imgs);
+        this.reservedId = reservedId;
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.adults = adults;
+        this.children = children;
+        this.infants = infants;
+    }
+
+    public Long getReservedId() {
+        return reservedId;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public int getAdults() {
+        return adults;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public int getInfants() {
+        return infants;
+    }
+}
