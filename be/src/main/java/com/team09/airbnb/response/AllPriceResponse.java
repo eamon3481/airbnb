cd714a01a9565e912b7a6d77aeb22c52253d451b
woody <woojihye2339@gmail.com>
2021-06-03 17:40:25 +0900

    [#25] fix : price 평균값 로직에 조건 추가
    > price 리스트 사이즈가 0일 경우
      - 바로 ZERO 값을 리턴하도록 로직 수정

--- be/src/main/java/com/team09/airbnb/response/AllPriceResponse.java
+++ be/src/main/java/com/team09/airbnb/response/AllPriceResponse.java
@@ -3,6 +3,7 @@ package com.team09.airbnb.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class AllPriceResponse {
@@ -26,19 +27,21 @@ public class AllPriceResponse {
     }
 
     private void setAvgPrice() {
-        BigDecimal sum = prices.stream()
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-        this.avgPrice = sum.divide(BigDecimal.valueOf(prices.size()));
+        if (!prices.isEmpty()) {
+            BigDecimal sum = prices.stream()
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+            this.avgPrice = sum.divide(BigDecimal.valueOf(prices.size()), RoundingMode.HALF_UP);
+        }
     }
 
-    private void setMinPrice(){
-        if(!prices.isEmpty()) {
+    private void setMinPrice() {
+        if (!prices.isEmpty()) {
             this.minPrice = prices.get(0);
         }
     }
 
-    private void setMaxPrice(){
-        if(!prices.isEmpty()){
+    private void setMaxPrice() {
+        if (!prices.isEmpty()) {
             this.maxPrice = prices.get(prices.size() - 1);
         }
     }
